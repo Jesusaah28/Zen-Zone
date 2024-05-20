c6be67cff01c1d3233b907946c7bce798ae3e682
Antonio Adame <132844285+Jesusaah28@users.noreply.github.com>
2024-05-20 00:01:59 -0600

    Update CM.js

--- Js/CM.js
+++ Js/CM.js
@@ -17,7 +17,11 @@ const numberWords = {
     'uno.': 1, 'dos.': 2, 'tres.': 3, 'cuatro.': 4, 'cinco.': 5,
     'seis.': 6, 'siete.': 7, 'ocho.': 8, 'nueve.': 9, 'diez.': 10,
     'once.': 11, 'doce.': 12, 'trece.': 13, 'catorce.': 14, 'quince.': 15,
-    'dieciseis.': 16, 'diecisiete.': 17, 'dieciocho.': 18, 'diecinueve.': 19, 'veinte.': 20
+    'dieciseis.': 16, 'diecisiete.': 17, 'dieciocho.': 18, 'diecinueve.': 19, 'veinte.': 20,
+        'uno': 1, 'dos': 2, 'tres': 3, 'cuatro': 4, 'cinco': 5,
+    'seis': 6, 'siete': 7, 'ocho': 8, 'nueve': 9, 'diez': 10,
+    'once': 11, 'doce': 12, 'trece.': 13, 'catorce': 14, 'quince': 15,
+    'dieciseis': 16, 'diecisiete': 17, 'dieciocho': 18, 'diecinueve': 19, 'veinte': 20
 };
 
 // Función para convertir texto a número
